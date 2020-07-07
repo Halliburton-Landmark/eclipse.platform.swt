a229bb63c59116bff5fd883d445f9eb9ad2da11c
Oleksandr Kozlov <Oleksandr.Kozlov@halliburton.com>
2020-07-07 15:51:32 +0300

    version update

--- bundles/org.eclipse.swt/Eclipse SWT Browser/win32/org/eclipse/swt/browser/IE.java
+++ bundles/org.eclipse.swt/Eclipse SWT Browser/win32/org/eclipse/swt/browser/IE.java
@@ -455,6 +455,7 @@ public void create(Composite parent, int style) {
 	OleListener oleListener = event -> {
 		/* callbacks are asynchronous, auto could be disposed */
 		if (auto != null) {
+		  if (browser.isDisposed()) return;
 			switch (event.type) {
 				case BeforeNavigate2: {
 
@@ -954,7 +955,6 @@ public void create(Composite parent, int style) {
 					if (arg13.getType() == OLE.VT_BSTR) {
 						String text = arg13.getString();
 						StatusTextEvent newEvent5 = new StatusTextEvent(browser);
-						if (browser.isDisposed()) return;
 						newEvent5.display = browser.getDisplay();
 						newEvent5.widget = browser;
 						newEvent5.text = text;
