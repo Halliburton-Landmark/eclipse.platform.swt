f7b864d81211bf88109ce20e89aadac25f93f0cf
Serhii Davydiuk <Serhii.Davydiuk@halliburton.com>
2021-07-01 16:16:35 +0300

    Backport fix for eclipse bug 548458
    https://github.com/eclipse/eclipse.platform.swt/commit/768f811d3f3b0d160d23f3a6e79f1760a9a64b24

--- bundles/org.eclipse.swt/Eclipse SWT PI/win32/org/eclipse/swt/internal/ole/win32/ICustomDestinationList.java
+++ bundles/org.eclipse.swt/Eclipse SWT PI/win32/org/eclipse/swt/internal/ole/win32/ICustomDestinationList.java
@@ -13,36 +13,34 @@
  *******************************************************************************/
 package org.eclipse.swt.internal.ole.win32;
 
-import org.eclipse.swt.internal.win32.*;
-
 public class ICustomDestinationList extends IUnknown {
 
 public ICustomDestinationList(long address) {
-    super(address);
+	super(address);
 }
 
 public int SetAppID(char[] pszAppID) {
-    return OS.VtblCall(3, address, pszAppID);
+	return COM.VtblCall(3, address, pszAppID);
 }
 
 public int BeginList(int[] pcMinSlots, GUID riid, long[] ppv) {
-    return COM.VtblCall(4, address, pcMinSlots, riid, ppv);
+	return COM.VtblCall(4, address, pcMinSlots, riid, ppv);
 }
 
 public int AppendCategory(char[] pszCategory, IObjectArray poa) {
-    return OS.VtblCall(5, address, pszCategory, poa.address);
+	return COM.VtblCall(5, address, pszCategory, poa.address);
 }
 
 public int AddUserTasks(IUnknown poa) {
-    return OS.VtblCall(7, address, poa.address);
+	return COM.VtblCall(7, address, poa.address);
 }
 
 public int CommitList() {
-    return OS.VtblCall(8, address);
+	return COM.VtblCall(8, address);
 }
 
 public int DeleteList(char[] pszAppID) {
-    return OS.VtblCall(10, address, pszAppID);
+	return COM.VtblCall(10, address, pszAppID);
 }
 
 }
